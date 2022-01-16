b3d1cf2d52a604ce898960b85ffe1e62d9b4cc12
An <phowflowf135@gmail.com>
2022-01-17 00:32:36 +0700

    remove some unuse param

--- src/app/home/admin/adminpage/adminpage.component.ts
+++ src/app/home/admin/adminpage/adminpage.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {UsersService} from "../../../service/user/users.service";
+import {ActivatedRoute} from "@angular/router";
+import {map, switchMap} from "rxjs";
 
 @Component({
   selector: 'app-adminpage',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./adminpage.component.scss']
 })
 export class AdminpageComponent implements OnInit {
+  totalpage: number[]=[];
+  listUser5: any[]=[];
 
-  constructor() { }
+  constructor(
+    private usersService: UsersService,
+    private activeRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+
+    // const paramMap = this.activeRoute.snapshot.paramMap;
+    // const page = paramMap.get('page')
+    // if(page){
+    //
+    // }
+    this.usersService.getAll().subscribe((data)=>{
+      for (let i = 1; i <= data.totalPages ; i++) {
+        this.totalpage.push(i);
+      }
+      console.log(this.totalpage);
+      this.listUser5=data.content;
+    })
   }
+
+
 
 }
